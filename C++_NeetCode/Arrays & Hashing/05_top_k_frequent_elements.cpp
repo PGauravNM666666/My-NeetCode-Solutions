08f7f99c3638a131827f5ff5cf205b74095591d2
PGNM <gauravnagamaheswar.p19@iiit.in>
2023-08-24 22:27:51 +0530

    comments update

--- C++_NeetCode/Arrays & Hashing/05_top_k_frequent_elements.cpp
+++ C++_NeetCode/Arrays & Hashing/05_top_k_frequent_elements.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+/*
+-> put a map with <int, int>
+-> initilie with 1 if the number is not present, otherwise, increase with 1
+-> put them in a vector of int pairs with frequency as first of the pair, so we can easy sort the vector
+-> reverse sort
+-> loop the vector of pairs for k times and print .second of each pair. 
+*/
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int, int> m;
         for(int i = 0; i < nums.size();i++)
