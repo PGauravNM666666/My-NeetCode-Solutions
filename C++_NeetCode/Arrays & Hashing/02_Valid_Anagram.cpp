503b0122c446f9c2cdd9ac1dbc5127e0f78a8538
PGNM <gauravnagamaheswar.p19@iiit.in>
2023-08-24 21:55:47 +0530

    Arrays and Hashing

--- C++_NeetCode/Arrays & Hashing/02_Valid_Anagram.cpp
+++ C++_NeetCode/Arrays & Hashing/02_Valid_Anagram.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
+        /*sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         if(s == t)
             return 1;
-        else return 0;
+        else return 0;*/
+
+       unordered_map<int, int> check1, check2;
+        for(int i=0;i<s.size();i++)
+        {
+            check1[s[i]]++;
+            
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            check1[t[i]]++;
+        }
     }
 };
