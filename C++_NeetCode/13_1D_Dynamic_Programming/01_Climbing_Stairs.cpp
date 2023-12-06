76709db3a8b70aa5eb4da5cb86cc5577303cd66d
PGNM <gauravnagamaheswar.p19@iiit.in>
2023-12-06 19:00:56 +0530

    Tree Problems etc..

---
+++ C++_NeetCode/13_1D_Dynamic_Programming/01_Climbing_Stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+   int climbStairs(int n) 
+   {
+      int one = 1,two = 1;
+      for(int i=0;i<n-1;i++)
+      {
+         int temp = one;
+         one  = one + two;
+         two  = temp;
+      }
+      return one;
+      
+      // int fib(int n)
+      // {
+      //     if(n == 0)
+      //         return 1;
+      //     if(n == 1)
+      //         return 1;
+      //     return fib(n-1) + fib(n-2);
+      // }
+      // return fib(n);
+   }
+};
