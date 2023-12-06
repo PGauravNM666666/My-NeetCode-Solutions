23711ebbad824a8c2cbef26258867759d44005d9
PGNM <gauravnagamaheswar.p19@iiit.in>
2023-12-06 19:05:34 +0530

    Linked List

--- C++_NeetCode/06_Linked_Lists/03_Remove_Nth_Node_From_End_of_List.cpp
+++ C++_NeetCode/06_Linked_Lists/03_Remove_Nth_Node_From_End_of_List.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
         // if(n == 1)
         // {
         //     ;
